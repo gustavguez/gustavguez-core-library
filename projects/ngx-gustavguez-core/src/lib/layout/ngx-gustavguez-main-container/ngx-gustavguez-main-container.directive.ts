7647152086ca116e9e8a1f086e4a8685ab958bd3
Gustavo Rodríguez <gustavguez@gmail.com>
2020-04-04 19:53:49 -0300

    fix(NgxGusavguezContainer): fix mobile toggle

--- projects/ngx-gustavguez-core/src/lib/layout/ngx-gustavguez-main-container/ngx-gustavguez-main-container.directive.ts
+++ projects/ngx-gustavguez-core/src/lib/layout/ngx-gustavguez-main-container/ngx-gustavguez-main-container.directive.ts
@@ -48,10 +48,13 @@ export class NgxGustavguezMainContainerDirective implements OnInit {
 		if (state) {
 			this.classes.push('sidebar-open');
 			this.classes.push('sidebar-collapse');
-		} else if (indexClassCollapse > -1){
-			this.classes.splice(indexClassCollapse, 1);
-		} else if (indexClassOpen > -1){
-			this.classes.splice(indexClassOpen, 1);
+		} else {
+			if (indexClassCollapse > -1) {
+				this.classes.splice(indexClassCollapse, 1);
+			}
+			if (indexClassOpen > -1) {
+				this.classes.splice(indexClassOpen, 1);
+			}
 		}
 
 		// Load classes
